0401b30b0c1ae61b325f53e3782b8d7a7fdd975a
xiaonangege <xxn19960511@163.com>
2018-11-18 00:21:47 +0800

    测试页面

--- post.php
+++ post.php
@@ -5,3 +5,10 @@
  * Date: 2018/11/17
  * Time: 23:26
  */
+require_once __DIR__."/../inc/global.php";
+
+$id = $_POST['input'];
+$sql = "select * from `user` where `id`={$id}";
+$db = Db::init('injection');
+$result = $db->fetchRows($sql);
+ajaxCallback(1,'test',$result);
